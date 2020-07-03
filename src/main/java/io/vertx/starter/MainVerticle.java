5a17ef10532a197e6fbaa0b961da078a845d72be
Hermann Josef Hill <7769946+hjhill@users.noreply.github.com>
2020-07-03 17:06:47 +0200

    Reproducer for problem with MultiMap.add

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,13 +1,21 @@
 package io.vertx.starter;
 
+import java.util.Collections;
+
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.http.HttpServerResponse;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start() {
     vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hello Vert.x!"))
+        .requestHandler(req -> {
+          final HttpServerResponse resp = req.response();
+          resp.headers().add(HttpHeaders.createOptimized("vary") , Collections.singleton(HttpHeaders.ORIGIN));
+          resp.end("Hello Vert.x!"); 
+        })
         .listen(8080);
   }
 
